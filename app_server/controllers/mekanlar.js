1c900460d415715b632b4b13de0dddb3f0b33c2d
SerhatG35 <genc.serhat97@gmail.com>
2021-01-17 14:17:35 +0300

    web2

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,6 +1,6 @@
 var request = require('postman-request');
 var apiSecenekleri = {
-  sunucu : "http://localhost:3000",
+  sunucu : "https://serhatgenc1821012036.herokuapp.com",
   apiYolu: '/api/mekanlar/'
 }
 var istekSecenekleri
